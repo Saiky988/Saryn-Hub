a226c65ee369396f52e791139ccc9bb29de588cd
Saiky988 <saikmusic988@gmail.com>
2025-04-11 21:32:37 +0700

    Update Saryn%Hub%Beta.lua

--- Saryn%Hub%Beta.lua
+++ Saryn%Hub%Beta.lua
@@ -7,7 +7,7 @@ local scripts = {
     [7018190066] = "https://raw.githubusercontent.com/Saiky988/Deadrails/refs/heads/main/DeadRails.lua", -- Lobby Dead Rails
     [70876832253163] = "https://raw.githubusercontent.com/Saiky988/Deadrails/refs/heads/main/DeadRails.lua", -- Game Dead Rails
     [6331902150] = "https://raw.githubusercontent.com/Saiky988/Forsaken/refs/heads/main/Forsaken-beta.lua", -- Forsaken
-    [7074860883] = "https://raw.githubusercontent.com/Saiky988/Arise-crs/refs/heads/main/Protected_6230802189214778.txt" --Arise Crossover
+    [7074860883] = "https://raw.githubusercontent.com/Saiky988/Arise/refs/heads/main/Protected_1685486258984862.txt" --Arise Crossover
 }
 
 if scripts[gameID] then
