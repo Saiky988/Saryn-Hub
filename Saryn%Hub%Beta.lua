5740572d32c8ef6dd985de875530cdf6f78f89c8
Saiky988 <saikmusic988@gmail.com>
2025-04-13 19:51:13 +0700

    Update Saryn%Hub%Beta.lua

--- Saryn%Hub%Beta.lua
+++ Saryn%Hub%Beta.lua
@@ -3,7 +3,7 @@ task.wait(1)
 local gameID = game.GameId
 
 local scripts = {
-    [5750914919] = 'https://raw.githubusercontent.com/Saiky988/Sarynhub-f/refs/heads/main/fisch.lua', -- Fisch
+    [5750914919] = 'https://raw.githubusercontent.com/Saiky988/Sarynhub-f/refs/heads/main/Protected_5077379096728738.txt', -- Fisch
     [7018190066] = "https://raw.githubusercontent.com/Saiky988/Deadrails/refs/heads/main/DeadRails.lua", -- Lobby Dead Rails
     [70876832253163] = "https://raw.githubusercontent.com/Saiky988/Deadrails/refs/heads/main/DeadRails.lua", -- Game Dead Rails
     [6331902150] = "https://raw.githubusercontent.com/Saiky988/Forsaken/refs/heads/main/Forsaken-beta.lua", -- Forsaken
