ba758374e82ecf583bc22ae28cabe4a780b8d638
Saiky988 <saikmusic988@gmail.com>
2025-05-04 16:53:34 +0700

    Update Saryn%Hub%Beta.lua

--- Saryn%Hub%Beta.lua
+++ Saryn%Hub%Beta.lua
@@ -1,10 +1,11 @@
 local gameID = game.GameId
 
 local scripts = {
-    [5750914919] = 'https://raw.githubusercontent.com/Saiky988/Sarynhub-f/refs/heads/main/Protected_5077379096728738.txt', -- Fisch
+    [5750914919] = 'https://raw.githubusercontent.com/Saiky988/Sarynhub-f/refs/heads/main/Protected_5077379096728738.txt', -- Fisch     https://raw.githubusercontent.com/Saiky988/Saryn-Hub/refs/heads/main/AnimeRangerX
     [7018190066] = "https://raw.githubusercontent.com/Saiky988/Deadrails/refs/heads/main/DeadRails.lua", -- Lobby Dead Rails
     [70876832253163] = "https://raw.githubusercontent.com/Saiky988/Deadrails/refs/heads/main/DeadRails.lua", -- Game Dead Rails
     [6331902150] = "https://raw.githubusercontent.com/Saiky988/Forsaken/refs/heads/main/Forsaken-beta.lua", -- Forsaken
+    [6884266247] = "https://raw.githubusercontent.com/Saiky988/Saryn-Hub/refs/heads/main/AnimeRangerX", -- AnimeRangerX 
     [7074860883] = "https://raw.githubusercontent.com/Saiky988/Arise/refs/heads/main/AriseSaryn.lua" --Arise Crossover
 }
 
